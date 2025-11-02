3e7dbf3c5adcce0ae20dcf77b9811f81dc87dc7a
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-11-02 20:55:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
-
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import './../styles/App.css';
+import Item1 from "./Item1";
+import Item2 from "./Item2";
+import Item3 from "./Item3";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <BrowserRouter>
+    <h1>Item List</h1>
+    <ul>
+      <li>
+        <Link to="/items/1">Item 1</Link>
+      </li>
+      <li>
+        <Link to="/items/2">Item 2</Link>
+      </li>
+      <li>
+        <Link to="/items/3">Item 3</Link>
+      </li>
+    </ul>
+
+    <Routes>
+      <Route path="/items/1" element={<Item1 />} />
+      <Route path="/items/2" element={<Item2 />} />
+      <Route path="/items/3" element={<Item3 />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
